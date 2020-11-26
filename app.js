3d5ecfadc5a485561a2de7fcde670b60642ecf4f
Justin <jmdinkelbach@gmail.com>
2020-11-26 21:10:25 +0000

    landing page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+// Set the view engine as ejs
+app.set("view engine", "ejs");
+
 // Routes
 app.get("/", function(req, res){
-    res.send("it works!");
-})
+    res.render("index");
+});
 
 // Start server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
-})
+});
