2d4edc9064f0b3d8bc571b434c0822363d61cd1e
jdinkelbach <jmdinkelbach@gmail.com>
2020-12-03 22:34:41 +0000

    Save keyword and photo url for database

--- public/js/function.js
+++ public/js/function.js
@@ -1,11 +1,30 @@
 /* global $ */
+/* global fetch */
+/* global URLSearchParams */
+
 $(document).ready(function(){
+    
+    /*  When clicking on a favorite icon, the icon changes color
+        and the image url is added/removed from the database. */
+    
     $(".favoriteIcon").on("click", function(){
-        if ($(this).attr("color") == "#666"){
+        let queryString = window.location.search;
+        let urlParams   = new URLSearchParams(queryString);
+        let keyword     = urlParams.get("keyword");
+        let imageUrl = $(this).prev().attr("src");
+        
+        if ($(this).attr("color") == "#666"){   //Favorite
             $(this).toggleClass("red");
+            updateFavorite("add", imageUrl, keyword);
         }
         else{
-            $(this).toggleClass("red");
+            $(this).toggleClass("red");         // Unfavorite
+            updateFavorite("remove", imageUrl, keyword);
         }
     })
+    
+    async function updateFavorite(action, imageUrl, keyword) {
+        let url = `/api/updateFavorites?action=${action}&imageUrl=${imageUrl}&keyword=${keyword}`;
+        await fetch(url);
+    }
 })
