ec9eec8a1f050b512a689d782be7d1a2bf415d23
jdinkelbach <jmdinkelbach@gmail.com>
2020-12-04 22:54:58 +0000

    added route for /api/updateFavorites

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+/* global pool */
+
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
+const pool = require("./dbPool.js");
 
 // Set the view engine as ejs
 app.set("view engine", "ejs");
@@ -34,9 +37,30 @@ app.get("/search", async function(req, res){
     res.render("results", {"imageUrl": data[0].urls.small, "imageUrlArray": imageUrlArray});
 })
 
+// Route for updating favorites api
+app.get("/api/updateFavorites", function(req, res){
+  let sql;
+  let sqlParams;
+  
+  // Add or delete
+  switch (req.query.action) {
+    case "add": sql = "INSERT INTO favorites (imageUrl, keyword) VALUES (?,?)";
+                sqlParams = [req.query.imageUrl, req.query.keyword];
+                break;
+    case "delete": sql = "DELETE FROM favorites WHERE imageUrl = ?";
+                sqlParams = [req.query.imageUrl];
+                break;
+  }//switch
+  pool.query(sql, sqlParams, function (err, rows, fields) {
+    if (err) throw err;
+    console.log(rows);
+    res.send(rows.affectedRows.toString());
+  });
+});//api/updateFavorites
+
 // Start server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
 });
 
-//https://api.unsplash.com/photos/?client_id=7WEnZ0-HH3el9avQVajOeFDCW3rKQBj-LmaxAk6I6GY
+
